4cf5af01f8cb31362d2252d9361253bc0627d49a
Vinay K <vinstrike1220@gmail.com>
2021-03-02 01:48:29 +0000

    awdawadad

---
+++ main.py
@@ -0,0 +1,31 @@
+#Author: Vinay K Date: 3-1-2021#
+import random
+
+colors = [
+    (0, 0, 0),
+    (120, 37, 179),
+    (100, 179, 179),
+    (80, 34, 22),
+    (80, 134, 22),
+    (180, 34, 22),
+    (180, 34, 122),
+]
+
+class Figure:
+    figures = [
+        [[1, 5, 9, 13], [4, 5, 6, 7]],
+        [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]],
+        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]],  
+        [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
+        [[1, 2, 5, 6]],
+    ]
+    def __init__(self, x, y):# the innit function tells what to do right after an object is created#
+        self.x = x
+        self.y = y
+        self.type = random.randint(0, len(self.figures) - 1)
+        self.color = random.randint(1, len(colors) - 1)
+        self.rotation = 0
+    def image(self):
+        return self.figures[self.type][self.rotation]#figures is a 3d array, type is the type of figure and rotation is the rotation of that figure#
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(self.figures[self.type])
